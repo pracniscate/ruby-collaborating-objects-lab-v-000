a7e61ae50278307aab437c3e8bbe5cc784023989
Lorena <lorena.giddens@outlook.com>
2018-06-16 15:28:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
     self.artist.add_song(self)
   end
 
-  def self.new_by_filename(filename)
-    song.name = filename.split(" - ")[1]
+  def self.new_by_filename(file)
+    song.name = file.split(" - ")[1]
     song = Song.new(song.name)
     song.artist_name = song.name[0]
     song
