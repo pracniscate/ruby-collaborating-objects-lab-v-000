317de5f164c456bdfeb51cd6941b99900305a0c7
Lorena <lorena.giddens@outlook.com>
2018-06-16 14:26:16 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+# parse all the filenames in the db/mp3 folder and send the filenames to the Song class
+
+class MP3Importer
+  attr_accessor :title
+
+  def files(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
+  end 
+
+  def import(list_of_filenames)
+    list_of_filenames.each{ |filename|
+  Song.new_by_filename(filename) }
+  end
+
+end  
