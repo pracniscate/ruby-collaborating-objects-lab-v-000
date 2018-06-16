c002f6b76b415d6d6403f8e7c11b1311bff5b7f7
Lorena <lorena.giddens@outlook.com>
2018-06-16 15:10:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,4 +5,12 @@ class Song
     @name = name
   end
 
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name 
+    end
+  end  
+
 end
